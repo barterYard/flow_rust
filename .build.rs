c1d15eb050bec5e116efe8eae6c9d1e9fc7e797b
Alexandre <alexandr.barbier@gmail.com>
2023-08-27 22:31:20 +0200

    rename build rs

--- .build.rs
+++ .build.rs
@@ -3,7 +3,6 @@ use std::process::{exit, Command};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let status = Command::new("buf").arg("generate").status().unwrap();
-
     if !status.success() {
         exit(status.code().unwrap_or(-1))
     }
